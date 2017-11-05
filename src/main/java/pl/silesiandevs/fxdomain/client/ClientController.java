21d93bfd1fdfe630cf23ec6ab67a33902f0373bc
Lukasz Ledwig <lukasz.ledwig@o2.pl>
2017-11-05 20:36:52 +0100

    Changed client's page part2

--- src/main/java/pl/silesiandevs/fxdomain/client/ClientController.java
+++ src/main/java/pl/silesiandevs/fxdomain/client/ClientController.java
@@ -5,10 +5,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import org.springframework.beans.factory.annotation.Autowired;
 import pl.silesiandevs.model.Customer;
 import pl.silesiandevs.repositories.CustomerRepository;
+
+import javax.xml.soap.Text;
 
 @FXMLController
 public class ClientController {
@@ -20,16 +23,46 @@ public class ClientController {
     private TextField streetName;
 
     @FXML
+    private TextField houseNumber;
+
+    @FXML
+    private TextField flatNumber;
+
+    @FXML
+    private TextField cityName;
+
+    @FXML
+    private TextArea comment; // ??? not sure if its a good name for this var // Pole: UWAGI
+
+    @FXML
+    private TextArea payment;
+
+    @FXML
     private Button saveBtn;
+
+    @FXML
+    private Button cancelBtn;
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    public void doContinue(ActionEvent actionEvent){
+        System.out.println("Dalej button clicked.");
+    }
+
+    public void doCancel(ActionEvent actionEvent){
+        System.out.println("Anuluj button clicked.");
+    }
 
     public void saveCustomer(ActionEvent actionEvent) {
         Customer c = new Customer();
         c.setPhoneNumber(phoneNumber.getText());
         c.setStreetName(streetName.getText());
-
+        c.setHouseNumber(houseNumber.getText());
+        c.setFlatNumber(flatNumber.getText());
+        c.setCityName(cityName.getText());
+        c.setComment(comment.getText());
+        c.setPayment(payment.getSelectedText().toString());
         System.out.println("creting customer " + c);
 
         customerRepository.save(c);
--- src/main/java/pl/silesiandevs/model/Customer.java
+++ src/main/java/pl/silesiandevs/model/Customer.java
@@ -15,6 +15,12 @@ public class Customer {
 
     private String phoneNumber;
     private String streetName;
+    private String houseNumber;
+    private String flatNumber;
+    private String cityName;
+    private String comment;
+    private String payment;
+
     private LocalDateTime lastOrderDateTime = LocalDateTime.now();
 
     public Long getId() {
@@ -41,6 +47,46 @@ public class Customer {
         this.streetName = streetName;
     }
 
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(String flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+
     public LocalDateTime getLastOrderDateTime() {
         return lastOrderDateTime;
     }
@@ -55,6 +101,9 @@ public class Customer {
         sb.append("id=").append(id);
         sb.append(", phoneNumber='").append(phoneNumber).append('\'');
         sb.append(", streetName='").append(streetName).append('\'');
+        sb.append(", houseNumber='").append(houseNumber).append('\'');
+        sb.append(", flatNumber='").append(flatNumber).append('\'');
+        sb.append(", cityName='").append(cityName).append('\'');
         sb.append(", lastOrderDateTime=").append(lastOrderDateTime);
         sb.append('}');
         return sb.toString();
